5d9aac2c869f4009709061367ba9953179eb304d
nekotribal <tribal.synesthesia@gmail.com>
2018-10-01 22:46:21 +0200

    -Added DataReader class for user input. -Rearranging classes for cleaner look.

--- src/org/nktrb/utils/DataReader.java
+++ src/org/nktrb/utils/DataReader.java
@@ -1,35 +1,30 @@
-package utils;
+package org.nktrb.utils;
 
-import data.Book;
+import org.nktrb.data.Book;
 
 import java.util.Scanner;
 
 public class DataReader {
-    private Scanner sc;
+    private static Scanner scanner = new Scanner(System.in);
 
     public DataReader() {
-        sc = new Scanner(System.in);
-    }
-
-    public void scannerClose() {
-        sc.close();
     }
 
     public Book readAndCreateBook() {
         System.out.println("Title: ");
-        String title = sc.nextLine();
+        String title = scanner.nextLine();
         System.out.println("Author: ");
-        String author = sc.nextLine();
+        String author = scanner.nextLine();
         System.out.println("Publisher: ");
-        String publisher = sc.nextLine();
+        String publisher = scanner.nextLine();
         System.out.println("ISBN: ");
-        String isbn = sc.nextLine();
+        String isbn = scanner.nextLine();
         System.out.println("Year Released: ");
-        int releaseDate = sc.nextInt();
-        sc.nextLine();
+        int releaseDate = scanner.nextInt();
+        scanner.nextLine();
         System.out.println("Pages: ");
-        int pages = sc.nextInt();
-        sc.nextLine();
+        int pages = scanner.nextInt();
+        scanner.nextLine();
 
         return new Book(title, author, releaseDate, pages, publisher, isbn);
     }
